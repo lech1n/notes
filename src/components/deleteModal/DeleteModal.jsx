6ec9a0c39d95ca9fcaf3d735bf7e7c421749b8c0
lechini <110564512+lech1n@users.noreply.github.com>
2024-01-19 18:27:23 +0100

    Update DeleteModal.jsx

--- src/components/deleteModal/DeleteModal.jsx
+++ src/components/deleteModal/DeleteModal.jsx
@@ -1,20 +1,29 @@
-import './DeleteModal.css'
+import './DeleteModal.css';
 
-const DeleteModal = ({ setShowModal , handleDelete }) => {
-	return (
-		<section className="deleteModal__container">
-		<h2>Are you sure you want to delete?</h2>
-		<div className="delete-btn-container">
-	  <button className="delete-btn"
-    onTouchEnd={() => { handleDelete(); setShowModal(false); }}
-    onClick={() => { handleDelete(); setShowModal(false); }}>
-     Yes
-   </button>
+const DeleteModal = ({ setShowModal, handleDelete }) => {
+  return (
+    <section className="deleteModal__container">
+      <h2>Are you sure you want to delete?</h2>
+      <div className="delete-btn-container">
+        <button
+          className="delete-btn"
+          onClick={() => {
+            handleDelete();
+            setShowModal(false);
+          }}
+        >
+          Yes
+        </button>
 
-  <button className="delete-btn" onTouchStart={() => setShowModal(false)}>No</button>
-		</div>
-		</section>
-		)
-}
+        <button
+          className="delete-btn"
+          onClick={() => setShowModal(false)}
+        >
+          No
+        </button>
+      </div>
+    </section>
+  );
+};
 
-export default DeleteModal
+export default DeleteModal;
